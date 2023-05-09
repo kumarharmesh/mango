041d3eef2fabbe005cef99827c92dc36aa7c9f42
Harmesh Kumar <kumarharmesh@hotmail.com>
2023-05-09 15:20:10 +0530

    new messages added

--- messages.cs
+++ messages.cs
@@ -2,4 +2,11 @@ public class Messages
 {
     public string Greet(){return "Hello, how are you";}
     public string Reprimand(){return "You have behaved improperly. Punishment will be meted out. ";}
+
+    public string Happy(){
+        return "I am very happy";
+    }
+    public string Sad(){
+        return "I am sad today";
+    }
 }
