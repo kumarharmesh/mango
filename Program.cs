7d944040c3c56a4cf0ed7df4a2c96d5fa620eb4b
kumarharmesh <kumarharmesh@gmail.com>
2023-05-09 16:36:17 +0530

    root code written

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 {
     static void Main(string[] args)
     {
-            Console.WriteLine(DateTime.Now);
+        double root;
+          if(args==null || args.Length==0){System.Console.WriteLine("Nothing to run"); return;}
+          foreach(string num in args)
+          {
+            root=Math.Sqrt(Convert.ToInt32(num));
+            System.Console.WriteLine($"Root of {num} is {root}");
+          }
     }
 }
