a8c3a6b431bc7f9ff3d28fa836b0d6d11c28874f
kumarharmesh <kumarharmesh@gmail.com>
2023-05-09 14:45:42 +0530

    Update maths.cs

--- maths.cs
+++ maths.cs
@@ -1,10 +1,16 @@
 public class DateClass
 {
     public string GetTime(){
-        return DateTime.Now.ToShortDateString();
+       string str=DateTime.Now.ToShortDateString();
+        return str;
     }
 
     public string GetDate(string format){
         return DateTime.Now.ToString(format);
     }
+    public int GetMonth
+    {
+        get=> DateTime.Now.Month;    
+    }
+
 }
